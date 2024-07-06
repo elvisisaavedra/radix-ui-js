05f1481df790566fdbe4dfd9ad6a38fd06a672fb
Elvis Saavedra <elvisisaavedra@yape.com.pe>
2024-07-06 04:46:37 -0500

    clean custom api

--- src/components/dialogCustomApi.jsx
+++ src/components/dialogCustomApi.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import {
   Dialog,
   DialogClose,
@@ -8,52 +7,34 @@ import {
 } from './ui/dialog'
 
 const DialogCustomAPI = () => {
-  const [isChecked, setIsChecked] = useState(false)
-
-  const handleOpenChange = () => {
-    setIsChecked(!isChecked)
-  }
-
   return (
-    <>
-      <Dialog open={isChecked} onOpenChange={handleOpenChange}>
-        <DialogTrigger>
-          Dialog Custom API
-        </DialogTrigger>
-        <DialogContent title={'Modal title'} onEscapeKeyDown={(event) => event.preventDefault()}>
-          <div className="modal-body">
-            <fieldset>
-              <label htmlFor="name">Name</label>
-              <input id="name" defaultValue="Pedro Duarte" />
-            </fieldset>
-            <fieldset>
-              <label htmlFor="username">Username</label>
-              <input id="username" defaultValue="@peduarte" />
-            </fieldset>
-          </div>
-          <DialogFooter>
-            <DialogClose asChild>
-              <button type="button" className="btn btn-secondary">
-                Close
-              </button>
-            </DialogClose>
-            <button type="button" className="btn btn-primary">
-              Save changes
+    <Dialog>
+      <DialogTrigger>
+        Dialog Custom API
+      </DialogTrigger>
+      <DialogContent title={'Modal title'}>
+        <div className="modal-body">
+          <fieldset>
+            <label htmlFor="name">Name</label>
+            <input id="name" defaultValue="Pedro Duarte" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="username">Username</label>
+            <input id="username" defaultValue="@peduarte" />
+          </fieldset>
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <button type="button" className="btn btn-secondary">
+              Close
             </button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-      {/* <label>
-        <input
-          type="checkbox"
-          name="topping"
-          value="Paneer"
-          checked={isChecked}
-          onChange={handleOpenChange}
-        />
-        Open Modal
-      </label> */}
-    </>
+          </DialogClose>
+          <button type="button" className="btn btn-primary">
+            Save changes
+          </button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
 
